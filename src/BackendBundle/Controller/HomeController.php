3c4bdf65bd80c949e34441444bcc0220e2fa937d
PapoMage <pablo.fernandez1@davinci.edu.ar>
2017-11-09 20:00:47 -0300

    Creacion del proyecto

---
+++ src/BackendBundle/Controller/HomeController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace BackendBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class HomeController extends Controller
+{
+    public function indexAction(Request $request)
+    {
+        return $this->render("home/index.html.twig");
+    }
+}
