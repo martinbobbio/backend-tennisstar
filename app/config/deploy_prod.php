8e084c254aa52ca314624f65baf6227272005693
mbobbio <mbobbio@varcelona.com>
2017-11-30 17:50:39 -0300

    fix deploy

---
+++ app/config/deploy_prod.php
@@ -0,0 +1,29 @@
+<?php
+
+use EasyCorp\Bundle\EasyDeployBundle\Deployer\DefaultDeployer;
+
+return new class extends DefaultDeployer
+{
+    public function configure()
+    {
+        return $this->getConfigBuilder()
+        ->server('u277128184@31.170.166.124:65002')
+        ->deployDir('/public_html/admin-tenis')
+        ->repositoryUrl('git@github.com:martinbobbio/davinci-tennisstar-backend.git')
+        ->repositoryBranch('master')
+    ;
+    }
+
+    // run some local or remote commands before the deployment is started
+    public function beforeStartingDeploy()
+    {
+        // $this->runLocal('./vendor/bin/simple-phpunit');
+    }
+
+    // run some local or remote commands after the deployment is finished
+    public function beforeFinishingDeploy()
+    {
+        // $this->runRemote('{{ console_bin }} app:my-task-name');
+        // $this->runLocal('say "The deployment has finished."');
+    }
+};
