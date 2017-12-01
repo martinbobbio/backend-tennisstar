8ab70701bc3ba1f2905ab4d61e3cba5b5667873f
martinbobbio1@gmail.com <mbobbio@varcelona.com>
2017-12-01 15:37:23 -0300

    fix

--- app/config/deploy_prod.php
+++ app/config/deploy_prod.php
@@ -7,8 +7,8 @@ return new class extends DefaultDeployer
     public function configure()
     {
         return $this->getConfigBuilder()
-        ->server('u277128184@31.170.166.124:65002')
-        ->deployDir('public_html/admin_tenis')
+        ->server('u277128184@admin-tenis.tennis-star.com')
+        ->deployDir('')
         ->repositoryUrl('git@github.com:martinbobbio/davinci-tennisstar-backend.git')
         ->repositoryBranch('master')
     ;
