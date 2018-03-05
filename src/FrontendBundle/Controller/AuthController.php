fa45b55a98f0eeb8e70fe13e3578e9ce3a594c97
Martin Bobbio <martinbobbio1@gmail.com>
2018-03-04 23:07:12 -0300

    endpoints de login y registro

---
+++ src/FrontendBundle/Controller/AuthController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace FrontendBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use FrontendBundle\Entity\ResponseRest;
+use BackendBundle\Entity\User;
+
+class AuthController extends Controller
+{
+    public function checkLoginAction(Request $request){
+
+        header("Access-Control-Allow-Origin: *");
+
+        $username = $request->get("username");
+        $password = $request->get("password");
+        
+        $user_manager = $this->get('fos_user.user_manager');
+        $factory = $this->get('security.encoder_factory');
+  
+        $user = $user_manager->loadUserByUsername($username);
+        $encoder = $factory->getEncoder($user);
+   
+        $bool = ($encoder->isPasswordValid($user->getPassword(),$password,$user->getSalt())) ? "true" : "false";
+
+        return ResponseRest::returnOk($bool);
+
+    }
+
+    public function newUserAction(Request $request){
+
+        header("Access-Control-Allow-Origin: *");
+
+        $username = $request->get("username");
+        $email = $request->get("email");
+        $password = $request->get("password");
+
+        $em = $this->getDoctrine()->getManager();
+        $usersRepository = $em->getRepository("BackendBundle:User");
+
+        $userManager = $this->get('fos_user.user_manager');
+        $email_exist = $userManager->findUserByEmail($email);
+        $username_exist = $userManager->findUserByUsername($username);
+
+        if($email_exist){
+            return ResponseRest::returnError("El email ya existe");
+        }
+        if($username_exist){
+            return ResponseRest::returnError("El nombre de usuario ya existe");
+        }
+
+        $user = new User();
+        $user->setEmail($email);
+        $user->setUsername($username);
+        $user->setEmailCanonical($email);
+        $user->setUsernameCanonical($username);
+        $user->setLocked(0);
+        $user->setEnabled(1);
+        $user->setPassword($this->container->get('security.encoder_factory')->getEncoder($user)->encodePassword($password, $user->getSalt()));
+
+        $em->persist($user);
+        $em->flush();
+
+        return ResponseRest::returnOk($request);
+
+    }
+
+
+
+}
