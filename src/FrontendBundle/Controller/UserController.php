2a3df4b068f95f874bb7915a13184101200d0460
Martin Bobbio <martinbobbio1@gmail.com>
2018-03-25 23:16:29 -0300

    nuevos endpoints

--- src/FrontendBundle/Controller/UserController.php
+++ src/FrontendBundle/Controller/UserController.php
@@ -27,6 +27,7 @@ class UserController extends Controller
         $user->getPlayerUser()->setFirstname($firstname);
         $user->getPlayerUser()->setLastname($lastname);
         $user->getPlayerUser()->setAge($age);
+        $user->setFullPlayer(true);
 
         $em->persist($user);
         $em->flush();
@@ -61,12 +62,87 @@ class UserController extends Controller
         $user->getSkillUser()->setVolley($volley);
         $user->getSkillUser()->setService($service);
         $user->getSkillUser()->setResistence($resistence);
+        $user->setFullGame(true);
 
         $em->persist($user);
         $em->flush();
 
         return ResponseRest::returnOk("ok");
 
+    }
+
+    public function getUserAction(Request $request){
+
+        header("Access-Control-Allow-Origin: *");
+
+        $id = $request->get("id");
+        
+        $em = $this->getDoctrine()->getManager();
+        $user = $em->getRepository('BackendBundle:User')->findOneById($id);
+
+        $arr = [];
+        $arr["username"] = $user->getUsername();
+        $arr["email"] = $user->getEmail();
+        $arr["firstname"] = $user->getPlayerUser()->getFirstname();
+        $arr["lastname"] = $user->getPlayerUser()->getLastname();
+        $arr["age"] = $user->getPlayerUser()->getAge();
+        $arr["gameStyle"] = $user->getSkillUser()->getGameStyle();
+        $arr["gameLevel"] = $user->getSkillUser()->getGameLevel();
+        $arr["backhandType"] = $user->getSkillUser()->getTypeBackhand();
+        $arr["forehand"] = $user->getSkillUser()->getForehand();
+        $arr["backhand"] = $user->getSkillUser()->getBackhand();
+        $arr["service"] = $user->getSkillUser()->getService();
+        $arr["volley"] = $user->getSkillUser()->getVolley();
+        $arr["resistence"] = $user->getSkillUser()->getResistence();
+
+        return ResponseRest::returnOk($arr);
+    }
+
+    public function getUserStatusAction(Request $request){
+
+        header("Access-Control-Allow-Origin: *");
+
+        $id = $request->get("id");
+        
+        $em = $this->getDoctrine()->getManager();
+        $user = $em->getRepository('BackendBundle:User')->findOneById($id);
+
+        $arr = [];
+        $arr["fullPlayer"] = $user->getFullPlayer();
+        $arr["fullGame"] = $user->getFullGame();
+
+        return ResponseRest::returnOk($arr);
+    }
+
+    public function getUsersRandomAction(Request $request){
+
+        header("Access-Control-Allow-Origin: *");
+
+        $id = $request->get("id");
+
+        $em = $this->getDoctrine()->getManager();
+        $users = $em->createQuery(
+        'SELECT u
+        FROM BackendBundle:User u
+        WHERE u.id != :id AND u.fullGame = 1
+        ')->setParameter('id', $id)
+        ->setMaxResults(3)->getResult();
+
+        $arr = [];
+        $arr1 = [];
+
+        foreach($users as $u){
+
+            $arr1['id'] = $u->getId();
+            $arr1['username'] = $u->getUsername();
+            $arr1['firstname'] = $u->getPlayerUser()->getFirstname();
+            $arr1['lastname'] = $u->getPlayerUser()->getLastname();
+            $arr1['gameStyle'] = $u->getSkillUser()->getGameStyle();
+            $arr1['gameLevel'] = $u->getSkillUser()->getGameLevel();
+            $arr[] = $arr1;
+        }
+
+        return ResponseRest::returnOk($arr);
     }
 
 
