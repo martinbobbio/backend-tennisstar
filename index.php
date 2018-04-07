551f5218c035618d0d1f19e4b2031e569725ae1c
Martin <martinbobbio1@gmail.com>
2018-04-07 17:46:37 +0000

    fix to prod

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
 <?php
 
 header("Location: web/app_dev.php");
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: X-Requested-With");
