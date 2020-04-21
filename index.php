adf7f5d5b18be1aa99674cf4f6f3343e1a4d8162
Martin Bobbio <martinbobbio1@gmail.com>
2020-04-21 10:50:58 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-header("Location: web/app_dev.php");
+header("Location: admin/stats");
 
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
